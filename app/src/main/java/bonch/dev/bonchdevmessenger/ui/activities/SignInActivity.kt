d7d407c41d9f387b28b9707402b46c2eeeccd6e6
Egridnev <egridnev@yandex.ru>
2019-10-19 01:07:31 +0300

    initial commit

---
+++ app/src/main/java/bonch/dev/bonchdevmessenger/ui/activities/SignInActivity.kt
@@ -0,0 +1,13 @@
+package bonch.dev.bonchdevmessenger.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bonch.dev.bonchdevmessenger.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
